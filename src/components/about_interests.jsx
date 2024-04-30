8da206d886249dfbaa80342fcae0e45624556344
Adam Halnon <halnon_01@hotmail.com>
2024-04-30 20:08:08 +0100

    fill out p tags with about and interests info, add classname

--- src/components/about_interests.jsx
+++ src/components/about_interests.jsx
@@ -5,14 +5,13 @@ export default function About() {
         <>
             <div className='about'>
             <h2>About</h2>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-                Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. 
-                Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus</p>
+            <p>Hi! My name is Adam, i'm a Fullstack Web Engineer with technical skills in writing web applications with React and Redux (...add back-end skills later). 
+                I am highly detail orientated and will help you in achieving the web app that best works for you and/or your clients</p>
 
-                <h2>Interests</h2>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-                Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. 
-                Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus</p>
+            <h2>Interests</h2>
+            <p id='interests'>I have eclectic tastes in music and films, and spend a lot of my time reading and excercising (which is super important for someone who sits down all day!) but most of the time,
+                I am striving to improve my proficiency in coding!
+            </p>
             </div>
         </>
     )
