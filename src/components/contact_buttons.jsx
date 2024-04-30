ba261e87c10416fe1d30aa8b0200f105b9749b79
Adam Halnon <halnon_01@hotmail.com>
2024-04-30 20:07:23 +0100

    fill out p tags with about and interests info, add classname

--- src/components/contact_buttons.jsx
+++ src/components/contact_buttons.jsx
@@ -4,12 +4,12 @@ export default function ContactButtons() {
     return (
         <>
             <div className='contact-buttons'>
-            <a href="#"><button id='email_button'><i className="fa-regular fa-envelope"></i>
-                Email
-            </button></a>
-            <a href='https://www.linkedin.com/in/adam-halnon-555649252/'><button id='linkedin_button'><i className="fa-brands fa-linkedin"></i>
-                LinkedIn
-            </button></a>
+                <a href="#"><button id='email_button'><i className="fa-regular fa-envelope"></i>
+                    Email
+                </button></a>
+                <a href='https://www.linkedin.com/in/adam-halnon-555649252/'><button id='linkedin_button'><i className="fa-brands fa-linkedin"></i>
+                    LinkedIn
+                </button></a>
             </div>
         </>
     )
