06b71e886c9238febe1ce83920f82a6d4fc61210
Adam Halnon <halnon_01@hotmail.com>
2024-04-30 20:31:49 +0100

    update path for img

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -4,7 +4,7 @@ export default function Profile() {
     return (
         <>
             <div className='profile'>
-                <img src="../src/assets/profilepic.png" alt="profile picture" />
+                <img src="./src/assets/profilepic.png" alt="profile picture" />
                 <h1>Adam Halnon</h1>
                 <h2>Fullstack Developer</h2>
                 <p>adam.halnon@example.com</p>
